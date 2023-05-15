545a0336081c1c5251390ddb62dc6c77661fb98f
azmifasya <azmifasya@student.telkomuniversity.ac.id>
2023-05-15 16:05:45 +0700

    nambahin function

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -23,4 +23,44 @@ class Pages extends BaseController
         echo view('pages/about');
         echo view('layout/footer');
     }
+
+    public function insight()
+    {
+        $data = [
+            'title' => 'Insight | Fabricity'
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/insight');
+        echo view('layout/footer');
+    }
+
+    public function services()
+    {
+        $data = [
+            'title' => 'Services | Fabricity'
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/services');
+        echo view('layout/footer');
+    }
+
+    public function page()
+    {
+        $data = [
+            'title' => 'Pages | Fabricity'
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/page');
+        echo view('layout/footer');
+    }
+
+    public function contact()
+    {
+        $data = [
+            'title' => 'Contact | Fabricity'
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/contact');
+        echo view('layout/footer');
+    }
 }
