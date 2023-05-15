86beaad712c32b1af521330a95f36a83b3efd567
azmifasya <azmifasya@student.telkomuniversity.ac.id>
2023-05-15 15:20:46 +0700

    first commit

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius repellendus quia deleniti atque mollitia natus veniam cum dolores eaque dolore similique totam hic, nemo ut iure ea accusamus molestias maxime.</p>
+        </div>
+    </div>
+</div>
