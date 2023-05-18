e0b75132a9a4c9c4fe8f59a3117077dcc4eeb2a5
TAdatafabric <januarikelaramin@gmail.com>
2023-05-19 01:37:07 +0700

    trying to call python script for dash plotly

--- app/Views/pages/insight.php
+++ app/Views/pages/insight.php
@@ -5,6 +5,10 @@
             <p>ini bisa diedit buat insight</p>
             <iframe src="http://localhost:3000/d-solo/be0f08ce-17f0-4a6c-9bdf-a7c1be83de8b/tadatafabric-dashboard-grafana?orgId=1&from=1610125200000&to=1628442000000&panelId=6" width="450" height="200" frameborder="0"></iframe>
                 <h3>Tes ini muncul kah?</h3>
+                <?php
+                $output = shell_exec("python insight.py");
+                echo $output;
+                ?> 
         </div>
     </div>
 </div>
