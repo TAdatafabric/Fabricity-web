9b8aa7004e3e7826e21a79a965db847ff1a44311
TAdatafabric <januarikelaramin@gmail.com>
2023-05-24 12:02:39 +0700

    Hapus head

--- app/Views/pages/insight.php
+++ app/Views/pages/insight.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col">
             <h1>Insight for Your Data</h1>
-<<<<<<< HEAD
+
             <p>Ini section untuk full visualisasi data</p>
 
 =======
@@ -15,7 +15,6 @@
             <iframe src="http://localhost:3010/d-solo/d06fce6d-645d-47ad-a3e5-2a38828d13d4/tadatafabric?orgId=1&from=1684719561610&to=1684741161610&panelId=2" width="450" height="200" frameborder="0"></iframe>
             
                 <h3>Rekomendasi service filter</h3>
->>>>>>> cb08970e12ffa5703cd9778e0a805116a407f83b
         </div>
     </div>
 </div>
