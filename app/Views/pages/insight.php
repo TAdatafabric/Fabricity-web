cce245f6d98228e39ac05539602f89efb96357eb
azmifasya <azmifasya@student.telkomuniversity.ac.id>
2023-06-11 01:56:15 +0700

    insight: visualisasi data

--- app/Views/pages/insight.php
+++ app/Views/pages/insight.php
@@ -5,9 +5,6 @@
     <div class="container insight-wrapper">
         <div class="col">
             <h1>Insight for Your Data</h1>
-
-            <div id="grafana-frame"></div>
-            <!-- }); -->
             </script>
             <iframe src="http://localhost:3010/d-solo/e9d71e09-5d6d-45ac-8aef-b13b21121e75/datafabric?orgId=1&from=1685412161741&to=1685433761741&panelId=5" frameborder="0"></iframe>
             <!-- map 2 -->
